9a6133ece61a4aecdb99f71ff50c58480473482a
Jimmy <jimmyyazji98@gmail.com>
2022-03-11 08:59:32 +0200

    Added router transition

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <Navigation />
   <main class="max-w-6xl mx-auto">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="scale" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </main>
 </template>
 <script setup>
 import Navigation from "@/components/MainNavigation.vue";
 </script>
+<style>
+scale-enter-active,
+.scale-leave-active {
+  transition: all 0.5s ease;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
+</style>
