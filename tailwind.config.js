9caed74ffa531b266ff9b2d3f8f3f2a0e5e33487
Jimmy <jimmyyazji98@gmail.com>
2022-03-11 07:50:42 +0200

    TailwindCss setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'gold': {
+          '50': "#ffff32",
+          '100': "#ffff28",
+          '200': "#fff51e",
+          '300': "#ffeb14",
+          '400': "#ffe10a",
+          '500': "#ffd700",
+          '600': "#f5cd00",
+          '700': "#ebc300",
+          '800': "#e1b900",
+          '900': "#d7af00",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
